377d429f3077a32f42dbd50742c679b6b76f1663
duck_ach <djagmlfk@naver.com>
2022-10-29 22:25:37 +0900

    2022-10-29

---
+++ practice.py
@@ -0,0 +1,23 @@
+#  숫자형 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+# 연산도 가능
+print(5+3)
+print(2*8)
+print(3*(3+4))
+# 문자형 자료형
+print('풍선') # 작은 따옴표 가능
+print("나비") # 큰 따옴표도 가능
+print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9) # 문자형과 계산을 섞어서 출력도 가능
+# boolean 자료형(true/false)
+print(5 > 10)       # True
+print(5 < 10)       # False
+print(True)         # True
+print(False)        # False
+# not 연산도 가능
+print(not True)     # False
+print(not False)    # True
+print(not (5 > 10)) # True
