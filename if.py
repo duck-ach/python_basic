b37e8ac562d6dd755fcac3656a28d093f66e3823
duck_ach <djagmlfk@naver.com>
2022-11-27 21:21:15 +0900

    2022-11-27

---
+++ if.py
@@ -0,0 +1,24 @@
+# if 조건 : 
+#     실행 명령문
+
+
+# 날씨에 따라 다르게 조건주기
+# weather = input("오늘 날씨는 어때요? ")
+
+# if weather == "비" :
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지" :
+#     print("마스크를 챙기세요")
+# else :
+#     print("준비물 필요 없어요")
+
+# 기온에 따라 다르게 조건주기
+temp = int(input("기온은 어때요? "))
+if 30 <= temp :
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30 :
+    print("괜찮은 날씨에요")
+elif 0 <= temp and temp < 10 :
+    print("외투를 챙기세요")
+else :
+    print("너무 추워요. 나가지 마세요")
